fcd22a1dbfeac5fa08bcbb541e58d8e403a8c26c
pratikp <pratik.patil@chitaledairy.com>
2026-04-02 17:54:17 +0530

    ...

--- client/src/components/InvoiceForm.jsx
+++ client/src/components/InvoiceForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import API from "../services/api";
-import { Button } from "react-bootstrap";
 
 export default function InvoiceForm({ onSuccess }) {
   const [customerName, setCustomerName] = useState("");
